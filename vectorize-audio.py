0bad02fd6c015fdbe186e9c8dc3a20ed8eade157
Marmik Patel <marmikp19@gmail.com>
2024-06-01 16:23:57 -0700

    store on mongodb atlas

--- vectorize-audio.py
+++ vectorize-audio.py
@@ -1,20 +1,10 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from deepgram import (
     DeepgramClient,
     PrerecordedOptions,
     FileSource
 )
-from langchain_community.document_loaders import TextLoader
-# from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS
-from langchain_core.prompts import ChatPromptTemplate
-# from langchain_groq import ChatGroq
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
-from langchain import hub
 import together
 import pymongo
 from typing import List
@@ -27,11 +17,7 @@ together.api_key = TOGETHER_API_KEY
 MONGO_DB_URI = os.getenv("MONGO_DB_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
-
-AUDIO_FILE = "./testing-audio/ice-cream.m4a"
-TITLE = "test youtube video title"
-
-EMBEDDING = "meta-llama/Llama-2-70b-chat-hf"
+MODEL = os.getenv("MODEL")
 
 def generate_embeddings(input_texts: List[str], model_api_string: str) -> List[List[float]]:
     """Generate embeddings from Together python library.
@@ -43,49 +29,68 @@ def generate_embeddings(input_texts: List[str], model_api_string: str) -> List[L
     Returns:
         embeddings_list: a list of embeddings. Each element corresponds to the each input text.
     """
-    together_client = together.Together()
-    outputs = together_client.embeddings.create(
-        input=input_texts,
-        model=model_api_string,
-    )
-    return [x.embedding for x in outputs.data]
+    try:
+        together_client = together.Together()
+        outputs = together_client.embeddings.create(
+            input=input_texts,
+            model=model_api_string,
+        )
+        return [x.embedding for x in outputs.data]
+    except Exception as e:
+        print(f"Exception: {e}")
 
 def store_embeddings(video_title: str, embedding: List[float]):
-    """Store the input text and its corresponding embedding into MongoDB."""
-    # Connect to MongoDB
-    mongo = pymongo.MongoClient(MONGO_DB_URI)
-    db = mongo.MONGO_DB_NAME
-    collection = db.MONGO_COLLECTION_NAME
-    
-    document = {
-        "text": video_title,
-        "embedding": embedding
-    }
-    
-    collection.insert_one(document)
+    """Store the input text and its corresponding embedding into MongoDB.
 
-try:
-    deepgram = DeepgramClient(DG_API_KEY)
+    Args:
+        video_title: str. the youtube video title.
+        embedding: List[float]. list of embeddings.
+    """
+    try:
+        mongo = pymongo.MongoClient(MONGO_DB_URI)
+        db = mongo[MONGO_DB_NAME]
+        collection = db[MONGO_COLLECTION_NAME]
+        
+        document = {
+            "text": video_title,
+            "embedding": embedding
+        }
+        
+        collection.insert_one(document)
+    except Exception as e:
+        print(f"Exception: {e}")
 
-    # transcribing the audio
+def vectorize_audio(video_title: str, audio_file):
+    """vectorize an audio file into mongodb atlas.
 
-    options = PrerecordedOptions(
-        model="nova-2",
-        smart_format=True,
-    )
+    Args:
+        video_title: str. the youtube video title.
+        audio_file: str. path to the audio file.
 
-    with open(AUDIO_FILE, "rb") as file:
+    Returns:
+        embeddings_list: a list of embeddings. Each element corresponds to the each input text.
+    """
+    try:
+        deepgram = DeepgramClient(DG_API_KEY)
+
+        options = PrerecordedOptions(
+            model="nova-2",
+            smart_format=True,
+        )
+
+        with open(audio_file, "rb") as file:
             buffer_data = file.read()
 
-    payload: FileSource = {
-        "buffer": buffer_data,
-    }
+        payload: FileSource = {
+            "buffer": buffer_data,
+        }
 
-    response = deepgram.listen.prerecorded.v("1").transcribe_file(payload, options)
+        # transcribing the audio
+        response = deepgram.listen.prerecorded.v("1").transcribe_file(payload, options)
 
-    # Vectorizing the transcription
-    output = generate_embeddings([response], EMBEDDING)
-    print(f"Embedding size is: {str(len(output[0]))}")
+        # Vectorizing the transcription
+        embeddings = generate_embeddings([response["results"]["channels"][0]["alternatives"][0]["transcript"]], MODEL)
+        store_embeddings(video_title, embeddings[0])
 
-except Exception as e:
-    print(f"Exception: {e}")
+    except Exception as e:
+        print(f"Exception: {e}")
